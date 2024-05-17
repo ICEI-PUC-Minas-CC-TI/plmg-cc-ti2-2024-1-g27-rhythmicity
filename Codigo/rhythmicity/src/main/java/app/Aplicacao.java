f96e1260fde5fbfcc863ad166119ecc1794e3c4d
fallypse <arhepk@gmail.com>
2024-05-17 08:50:03 -0300

    teste

--- Codigo/rhythmicity/src/main/java/app/Aplicacao.java
+++ Codigo/rhythmicity/src/main/java/app/Aplicacao.java
@@ -18,12 +18,13 @@ public class Aplicacao {
             response.redirect("forms.html");
             return null;
         });
-                
+                        
         // Rotas de login e cadastro
         post("/usuario/signup", (request, response) -> userService.signup(request, response));
         post("/usuario/login", (request, response) -> userService.login(request, response));
 
         // Rotas CRUD
+        get("/usuario/insert/*", (request, response) -> userService.get(request, response));
         get("/usuario/:id", (request, response) -> userService.get(request, response));
         get("/usuario/list/:orderby", (request, response) -> userService.getAll(request, response));
         put("/usuario/update/:id", (request, response) -> userService.update(request, response));
